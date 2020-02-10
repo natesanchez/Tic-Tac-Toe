501d905bb40398423d050c08de35314281fa98c0
Nate_Sanchez <NSanchez@usa.com>
2020-02-10 10:54:40 -0500

    dom selectors

---
+++ script.js
@@ -0,0 +1,17 @@
+let box1 = document.querySelector(".box1")
+
+let box2 = document.querySelector(".box2")
+
+let box3 = document.querySelector(".box3")
+
+let box4 = document.querySelector(".box4")
+
+let box5 = document.querySelector(".box5")
+
+let box6 = document.querySelector(".box6")
+
+let box7 = document.querySelector(".box7")
+
+let box8 = document.querySelector(".box8")
+
+let box9 = document.querySelector(".box9")
