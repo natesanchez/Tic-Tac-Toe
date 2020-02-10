1980793a9aae410ef2c1c088a343cf127bdbda52
Nate_Sanchez <NSanchez@usa.com>
2020-02-10 12:10:31 -0500

    commit

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 let boxes = document.querySelectorAll(".boxes")
 
-let box1 = document.querySelector(".box1")
 
-let box2 = document.querySelector(".box2")
+let gameDiv = document.querySelector(".gameArea");
 
-let box3 = document.querySelector(".box3")
 
-let box4 = document.querySelector(".box4")
+let gameTracker = 0
 
-let box5 = document.querySelector(".box5")
-
-let box6 = document.querySelector(".box6")
-
-let box7 = document.querySelector(".box7")
-
-let box8 = document.querySelector(".box8")
-
-let box9 = document.querySelector(".box9")
 
 boxes.forEach(item => {
 	item.addEventListener("click", changeColor)
 })
 
+
+function tracker () {
+	gameTracker = gameTracker + 1;
+}
+
+
 function changeColor (e) {
 	e.preventDefault();
-	let id = event.target.getAttribute("data-id")
-	console.log(id)
+	if (gameTracker % 2 === 0) {
+		e.target.style.background = "red";
+		tracker();
+	} else {
+		e.target.style.background = "blue";
+		tracker();
+	}
+	e.target.removeEventListener("click", changeColor)
 }
+
+
