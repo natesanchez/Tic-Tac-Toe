16e5cc39ab15d5e59e8a8e8ed05905656d1fbc3f
Nate_Sanchez <NSanchez@usa.com>
2020-02-10 11:09:26 -0500

    dom work

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let boxes = document.querySelectorAll(".boxes")
+
 let box1 = document.querySelector(".box1")
 
 let box2 = document.querySelector(".box2")
@@ -15,3 +17,13 @@ let box7 = document.querySelector(".box7")
 let box8 = document.querySelector(".box8")
 
 let box9 = document.querySelector(".box9")
+
+boxes.forEach(item => {
+	item.addEventListener("click", changeColor)
+})
+
+function changeColor (e) {
+	e.preventDefault();
+	let id = event.target.getAttribute("data-id")
+	console.log(id)
+}
